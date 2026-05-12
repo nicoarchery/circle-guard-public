6a9053c036163e7836723d31adc79fae09a3292d
nicoarchery <nicojimev1234@gmail.com>
2026-05-11 19:00:34 -0500

    jenkins modificaations

--- services/circleguard-auth-service/src/main/java/com/circleguard/auth/controller/LoginController.java
+++ services/circleguard-auth-service/src/main/java/com/circleguard/auth/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
-
+//ggggg
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
